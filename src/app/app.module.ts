eac84fdcf1f48e56e8c54ede52445baae01b908d
jhonmcc <jhonmcc@gmail.com>
2022-08-15 12:18:44 -0300

    criado componentes do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookCartComponent } from './components/book-cart/book-cart.component';
+import { BooksListComponent } from './components/book-cart/books-list/books-list.component';
+import { SearchFilterComponent } from './components/book-cart/search-filter/search-filter.component';
+import { BookItemComponent } from './components/book-cart/book-list/book-item/book-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    BookCartComponent,
+    BooksListComponent,
+    SearchFilterComponent,
+    BookItemComponent
   ],
   imports: [
     BrowserModule,
